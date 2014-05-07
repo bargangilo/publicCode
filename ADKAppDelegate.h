b2b99222b62ff04f74398415b73f8ec8c7491913
bargangilo <king.donovan@gmail.com>
2014-05-07 10:31:08 -0500

    App used to teach basic Core Data to CocoaNuts

---
+++ ADKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ADKAppDelegate.h
+//  Out To Eat
+//
+//  Created by Donovan King on 4/30/14.
+//  Copyright (c) 2014 Donovan King. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ADKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
